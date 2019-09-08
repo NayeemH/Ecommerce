1951026d1e314cc1acf6a5000080ad1689abbff6
NayeemH <nayeemh67@gmail.com>
2019-09-09 02:19:12 +0600

    starts the project

--- index.php
+++ index.php
@@ -5,13 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LGED ECOMMERCE</title>
+    <link rel="stylesheet" href="styles/style.css" media= "all"/>
 </head>
 <body>
-    <div>
-        <div>Header Section</div>
-        <div>Navigation Section</div>
-        <div>Content Section</div>
-        <div>Footer Section</div>
+               <!-- main container -->
+    <div class="main_wrapper">  
+
+    <!-- header starts from here -->
+        <div class="header_wrapper">
+            <img src="images/logo.png" alt="" style="float:left;"> 
+            <img src="images/adgif.gif" alt="" style="float:right;">
+        </div>
+        <!-- header ends here -->
+
+        <!-- navigation bar starts -->
+        <div id="navbar">Navigation Section</div>
+        <!-- navigation bar ends -->
+
+        <!-- contents starts -->
+        <div class="content_wrapper">
+            <div id="left_sidebar">Left side</div>
+            <div id="right_content">right side</div>
+        </div>
+        <!-- contents ends -->
+
+
+        <div class="footer">footer Section</div>
     
     </div>
 </body>
